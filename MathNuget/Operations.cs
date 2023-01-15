e271c929be107a017dba383b1ef8ea549d354c49
NIMIDEV <99932058+NIMICODER@users.noreply.github.com>
2023-01-15 16:40:31 +0100

    Add project files.

---
+++ MathNuget/Operations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MathNuget
+{
+    public class Operations
+    {
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtraction(int num1, int num2)
+        {
+            return (num1 - num2);
+        }
+
+        public int Division(int num1, int num2)
+        {
+            return (num1 / num2);
+        }
+
+        public int Multiplication(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+        //Complex Operations
+        public double RaiseToPower(double num, int power)
+        {
+            return Math.Pow(num, power);
+        }
+
+        public double Square(double num)
+        {
+            return Math.Pow(num, 2);
+        }
+
+
+        public double Hypotenuse(double num1, double num2)
+        {
+            return Math.Sqrt(Math.Pow(num1, 2) + Math.Pow(num2, 2));
+        }
+
+
+
+
+    }
+}
